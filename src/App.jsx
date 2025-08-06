d84b49609af08cec1f84bbca6d7d9a9525e3943a
Arjunlama2 <instructorarjun1@gmail.com>
2025-08-06 17:57:44 +0545

    setup: initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import Layout from "./layout/Layout";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<div>THis is home page</div>} />
+          <Route path="products" element={<div>THis is all product page</div>} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
