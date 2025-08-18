6a4c40c4383fddee66772a265e845106738bc6cd
Arjunlama2 <instructorarjun1@gmail.com>
2025-08-18 17:29:21 +0545

    updates

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Outlet } from 'react-router'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { useDispatch } from 'react-redux'
 import { setUser } from '../redux/Reducers/userSlice'
+import axios from 'axios'
+import useFetch from '../hook/useFetch'
 
 function Layout() {
   const dispatch = useDispatch()
+
+const {data}=useFetch("product")
+
+
+console.log(data)
 
   return (
     <>
